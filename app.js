953f8655d331bc361f6d04e68dbb232f633094a0
Akshat-Gup <69629823+Akshat-Gup@users.noreply.github.com>
2020-12-20 11:57:42 +0800

    created collapsible lists

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const addButton = document.querySelector(".addButton");
 const toDoList = document.querySelector(".todos");
 const finishedToDos = document.querySelector(".finishedtodos");
-
+const searchBar = document.querySelector(".search input");
+const coll = document.getElementsByClassName("collapsible");
 // Function used to create a to do 
 const createToDo = value => {
 	const htmlTemplate = `
@@ -11,8 +12,8 @@ const createToDo = value => {
 	</li>
 	`;
 	toDoList.innerHTML += htmlTemplate;
-}
-
+};
+// Function used to add a selected to-do to another place
 const addRemoveToDoHtml = innerHtml => {
 	const newHtml = `
 	<li class="list-group-item d-flex justify-content-between align-items-center mb-1">
@@ -20,6 +21,18 @@ const addRemoveToDoHtml = innerHtml => {
 	</li>`;
 	return newHtml;
 }
+
+const filterToDos = search => {
+
+	let incompleteTasks = Array.from(toDoList.children);
+	let completedTasks = Array.from(finishedToDos.children);
+	let tasks = incompleteTasks.concat(completedTasks);
+
+	
+	tasks.filter(_ => {
+
+	});
+};
 
 /* This piece of code creates a new to do based off the HTML template and the user sumbitted value
 once the submit button is pressed*/
@@ -62,3 +75,22 @@ finishedToDos.addEventListener("click", event => {
 			event.target.parentElement.remove();
 	}
 });
+
+searchBar.addEventListener("keyup", _ => {
+	const searchTerm = searchBar.value.trim();
+	filterToDos(searchTerm);
+});
+
+//collapsible functionality
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
